2eac3ff3f09c81017dc13982ec13b7ca9edbd147
biky-mandal <m.biky50@gmail.com>
2021-08-06 03:11:41 +0530

    Ui updated

--- src/component/votdiv/index.js
+++ src/component/votdiv/index.js
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import img1 from '../../resources/L1/L1GS.png';
 import './style.css';
 
 
 const VoteDiv = (props) => {
     console.log(props.candidate)
+
+    const [ value, setValue ] = useState();
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    console.log(value);
     return(
         <div className="voting-div"> 
             <div className="l-div">
-                <img src={img1}/>
+                <img src={props.candidate.L1.imageURL}/>
                 <label className="c-name">{props.candidate.L1.fullName}</label>
                 <label className="c-post">{props.candidate.L1.position}</label>
+
+                <div className="wrapper">
+                    <input 
+                        type="radio" 
+                        checked={value === props.candidate.L1.fullName} 
+                        value={props.candidate.L1.fullName} 
+                        onChange={handleChange} 
+                        name={props.candidate.L1.position} 
+                        id={props.candidate.L1.fullName}
+                    />
+                    <label for={props.candidate.L1.fullName} class="option option-1">
+                        <span>{value === props.candidate.L1.fullName ? 'Voted' : 'Vote'}</span>
+                    </label>
+                </div>
             </div>
             <div className="l-div">
-                <img src={img1}/>
+                <img src={props.candidate.L2.imageURL}/>
                 <label className="c-name">{props.candidate.L2.fullName}</label>
                 <label className="c-post">{props.candidate.L2.position}</label>
+
+                <div className="wrapper">
+                    <input 
+                        type="radio" 
+                        checked={value === props.candidate.L2.fullName} 
+                        value={props.candidate.L2.fullName} 
+                        onChange={handleChange} 
+                        name={props.candidate.L2.position} 
+                        id={props.candidate.L2.fullName}
+                    />
+                    <label for={props.candidate.L2.fullName} class="option option-2">
+                        <span>{value === props.candidate.L2.fullName ? 'Voted' : 'Vote'}</span>
+                    </label>
+                </div>
             </div>
         </div>
     );
--- src/container/homePage/index.js
+++ src/container/homePage/index.js
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { LogoutAction } from '../../redux/actions/authAction';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 
 import {HiMenuAlt3} from 'react-icons/hi';
 import {FiX, FiLogOut} from 'react-icons/fi';
 import {GoHome} from 'react-icons/go';
-import './style.css';
 import VoteDiv from '../../component/votdiv';
 import { getCandidateAction } from '../../redux/actions/candidateAction';
+
+import './style.css';
+import Loader from 'react-loader-spinner';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 /**
 * @author
 * @function HomePage
@@ -28,6 +31,10 @@ const HomePage = (props) => {
     dispatch(LogoutAction())
   }
 
+  const redirectToHome = () => {
+    <Redirect to="/" />
+  }
+
   useEffect(() => {
     dispatch(getCandidateAction());
   },[])
@@ -35,13 +42,12 @@ const HomePage = (props) => {
 
   return(
     <div className="homepage-main-div">
-      {/* <NavLink to="/login" onClick={logoutFunction}>Logout</NavLink> */}
       <div className="heading-div">
         <label className="logo-name">JECvotingSystem</label>
         <div className="menu-Item">
           <div className="menu-icons"><HiMenuAlt3/></div>
-          <div className="menu-icons"><GoHome/></div>
-          <div className="menu-icons"><FiLogOut/></div>
+          <div className="menu-icons" onClick={redirectToHome}><GoHome/></div>
+          <div className="menu-icons" onClick={logoutFunction}><FiLogOut/></div>
         </div>
       </div>
 
@@ -57,7 +63,12 @@ const HomePage = (props) => {
               return <VoteDiv candidate={candidate}/>
             })
             :
-            null
+            <Loader
+              type="Puff"
+              color="#00BFFF"
+              height={100}
+              width={100}
+            />
           }
         </div>
       </div>
